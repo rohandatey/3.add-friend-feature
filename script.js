2061531bbde624c4d4f6b467b2ffc965f61572b9
rohandatey@gmail.com <rohandatey@gmail.com>
2026-09-02 22:26:36 +0530

    single button

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-let istates = document.querySelector("h5");
+// let istatus = document.querySelector("h5");
+// let addFriend = document.querySelector("#add");
+// let removeFriend = document.querySelector("#remove");
+
+// addFriend.addEventListener("click", () => {
+//   istatus.innerHTML = "Friends"; // here i chnage inner HTML
+//   istatus.style.color = "green"; // here i chnage inner HTML color
+// });
+
+// removeFriend.addEventListener("click", () => {
+//   istatus.innerHTML = "Stranger"; // here i chnage inner HTML
+//   istatus.style.color = "red";
+// });
+
+let istatus = document.querySelector("h5");
 let addFriend = document.querySelector("#add");
-let removeFriend = document.querySelector("#remove");
+
+let check = 0;
 
 addFriend.addEventListener("click", () => {
-  istates.innerHTML = "friends";
-  istates.style.color = "green";
-});
-removeFriend.addEventListener("click", () => {
-  istates.innerHTML = "stranger";
-  istates.style.color = "red";
+  //if main == use hoga == use true/false ke liye hota ha
+  // and = oprater set ke kiye hota ha
+
+  if (check == 0) {
+    istatus.innerHTML = "Friends";
+    istatus.style.color = "green";
+    check = 1;
+  } else {
+    istatus.innerHTML = "Stranger";
+    istatus.style.color = "red";
+    check = 0;
+  }
 });
