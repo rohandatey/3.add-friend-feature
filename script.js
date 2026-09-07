0fd91ea6422fe8681ce5b5d0f736a3496760d83a
rohandatey@gmail.com <rohandatey@gmail.com>
2026-09-07 22:20:12 +0530

    cursor

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-// let istatus = document.querySelector("h5");
-// let addFriend = document.querySelector("#add");
-// let removeFriend = document.querySelector("#remove");
+let main = document.querySelector("#main");
+let cursor = document.querySelector(".cursor");
 
-// addFriend.addEventListener("click", () => {
-//   istatus.innerHTML = "Friends"; // here i chnage inner HTML
-//   istatus.style.color = "green"; // here i chnage inner HTML color
-// });
+// cursor.style.backgroundColor = "red"
 
-// removeFriend.addEventListener("click", () => {
-//   istatus.innerHTML = "Stranger"; // here i chnage inner HTML
-//   istatus.style.color = "red";
-// });
-
-let istatus = document.querySelector("h5");
-let addFriend = document.querySelector("#add");
-
-let check = 0;
-
-addFriend.addEventListener("click", () => {
-  //if main == use hoga == use true/false ke liye hota ha
-  // and = oprater set ke kiye hota ha
-
-  if (check == 0) {
-    istatus.innerHTML = "Friends";
-    istatus.style.color = "green";
-    check = 1;
-  } else {
-    istatus.innerHTML = "Stranger";
-    istatus.style.color = "red";
-    check = 0;
-  }
+main.addEventListener("mousemove", (dets) => {
+  console.log(dets);
+  cursor.style.left = dets.x + "px";
+  cursor.style.top = dets.y + "px";
 });
